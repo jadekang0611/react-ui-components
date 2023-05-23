558d4b411e0bc2ea5f7953dca50f2b8adddcfa63
jadekang0611 <56938630+jadekang0611@users.noreply.github.com>
2023-05-22 17:05:05 -0700

    feat: implemented the skseleton ui and demo

--- skeleton-ui/src/App.tsx
+++ skeleton-ui/src/App.tsx
@@ -1,5 +1,106 @@
+import styled from "@emotion/styled/macro";
+import Skeleton from "./components/Skeleton";
+import DummyImage from "./assets/dummy-img.jpg";
+import { useEffect, useState } from "react";
+
+const Base = styled.div`
+  display: grid;
+  width: 100%;
+  grid-template-columns: repeat(5, 1fr);
+  column-gap: 12px;
+  row-gap: 12px;
+
+  @media (max-width: 768px) {
+    grid-template-columns: repeat(3, 1fr);
+  }
+
+  @media (max-width: 480px) {
+    grid-template-columns: repeat(2, 1fr);
+  }
+`;
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  border-radius: 6px;
+`;
+const ImageWrapper = styled.div`
+  width: 100%;
+`;
+const Image = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  border-radius: 6px;
+`;
+const Info = styled.div`
+  padding: 1rem;
+  display: flex;
+  flex-direction: column;
+  flex: 1 1 0%;
+`;
+const Title = styled.h4`
+  margin: 0;
+  padding: 0;
+  font-size: 24px;
+`;
+const Description = styled.p`
+  margin: 8px 0 0 0;
+  padding: 0;
+  font-size: 16px;
+`;
+
+const Placeholder: React.FC = () => {
+  return (
+    <Container>
+      <ImageWrapper>
+        <Skeleton width={420} height={220} />
+      </ImageWrapper>
+      <Info>
+        <Skeleton width={150} height={32} rounded />
+        <div style={{ height: "8px" }} />
+        <Skeleton width={300} height={22} rounded />
+      </Info>
+    </Container>
+  );
+};
+
+const Item: React.FC = () => {
+  return (
+    <Container>
+      <ImageWrapper>
+        <Image src={DummyImage} />
+        {/* Photo by The Lazy Artist Gallery: https://www.pexels.com/photo/man-wearing-pink-suit-jacket-holding-using-tablet-computer-1303862/ */}
+      </ImageWrapper>
+      <Info>
+        <Title>Nulla et magna est nulla.</Title>
+        <Description>
+          Ad minim enim ex ad ex dolore.Laboris amet laboris elit amet
+          incididunt. Excepteur mollit ipsum anim exercitation dolor. Cillum
+          magna consequat duis ipsum ex reprehenderit proident minim.
+        </Description>
+      </Info>
+    </Container>
+  );
+};
+
 function App() {
-  return <div className="App">Hello Skeleton</div>;
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => setLoading(false), 3000);
+  }, []);
+  return (
+    <div className="App">
+      <Base>
+        {loading
+          ? Array.from({ length: 25 }).map((_, idx) => (
+              <Placeholder key={idx} />
+            ))
+          : Array.from({ length: 25 }).map((_, idx) => <Item key={idx} />)}
+      </Base>
+    </div>
+  );
 }
 
 export default App;
--- skeleton-ui/src/components/Skeleton.tsx
+++ skeleton-ui/src/components/Skeleton.tsx
@@ -1,5 +1,6 @@
 import styled from "@emotion/styled/macro";
 import { keyframes, css } from "@emotion/react";
+import { useMemo } from "react";
 
 interface Props {
   width?: number;
@@ -29,24 +30,45 @@ const pulseAnimation = css`
   animation: ${pulseKeyframe} 1.5s ease-in-out infinite;
 `;
 
-const Base = styled.div`
+const Base = styled.div<Props>`
   ${({ color }) => color && `background-color: ${color}`};
-  ${({ rounded }) => rounded && "border-radius: 8px"}
-  ${({ circle }) => circle && "border-radius: 50%"}
-  ${({ width, height }) => (width || height) && "display: block"}
-  ${({ animation }) => animation && pulseAnimation}
-  width: ${({ width, unit }) => width && unit && `${width}${unit}`}
-  height: ${({ height, unit }) => height && unit && `${height}${unit}`}
+  ${({ rounded }) => rounded && "border-radius: 8px"};
+  ${({ circle }) => circle && "border-radius: 50%"};
+  ${({ width, height }) => (width || height) && "display: block"};
+  ${({ animation }) => animation && pulseAnimation};
+  width: ${({ width, unit }) => width && unit && `${width}${unit}`};
+  height: ${({ height, unit }) => height && unit && `${height}${unit}`};
 `;
 
 const Content = styled.span`
   opacity: 0;
 `;
 
-const Skeleton: React.FC<Props> = () => {
-  const content = "";
+const Skeleton: React.FC<Props> = ({
+  width,
+  height,
+  circle,
+  rounded,
+  count,
+  unit = "px",
+  color = "#F4F4F4",
+  style,
+}) => {
+  // ex) count 4 => '----'
+  const content = useMemo(
+    () => [...Array({ length: count })].map(() => "-").join(""),
+    [count]
+  );
   return (
-    <Base>
+    <Base
+      style={style}
+      rounded={rounded}
+      circle={circle}
+      height={height}
+      width={width}
+      unit={unit}
+      color={color}
+    >
       <Content>{content}</Content>
     </Base>
   );
