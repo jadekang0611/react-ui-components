dd07838c437d50d60b6c8bb6c1f9b86eb80bd722
jadekang0611 <56938630+jadekang0611@users.noreply.github.com>
2023-05-24 15:24:08 -0700

    feat: connect dummy api to demo pagination and update ui

--- pagination/src/App.tsx
+++ pagination/src/App.tsx
@@ -1,7 +1,96 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Pagination from "./components/Pagination";
+import UserCard from "./components/UserCard";
+import { Passenger, Response } from "./interface/Api";
+import styled from "@emotion/styled/macro";
+
+const AppWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 10px 20px;
+`;
+
+const PageTitle = styled.h2`
+  font-size: 24px;
+  text-align: center;
+  font-weight: bold;
+  font-family: "Quicksand", sans-serif;
+`;
+
+const UserList = styled.ul`
+  list-style: none;
+  display: grid;
+  grid-template-columns: repeat(1, 1fr); // Default: 1 column
+  padding-left: 0px;
+  @media (min-width: 768px) {
+    grid-template-columns: repeat(2, 1fr); // 2 columns from 768px and above
+  }
+
+  @media (min-width: 1024px) {
+    grid-template-columns: repeat(3, 1fr); // 3 columns from 1024px and above
+  }
+
+  @media (min-width: 1200px) {
+    grid-template-columns: repeat(4, 1fr); // 4 columns from 1200px and above
+  }
+
+  column-gap: 10px;
+  row-gap: 12px;
+`;
+
+const PaginationWrapper = styled.div`
+  margin-top: 20px; // Pushes the Pagination component to the bottom
+`;
 
 function App() {
-  return <div className="App">Pagination</div>;
+  const [page, setPage] = useState(0);
+  const [totalPages, setTotalPages] = useState(0);
+  const [items, setItems] = useState<Array<Passenger>>([]);
+
+  const handlePageChange = (currentPage: number): void => {
+    setPage(currentPage);
+  };
+
+  useEffect(() => {
+    const fetch = async () => {
+      const params = { page, size: 16 };
+      const {
+        data: { totalPages, data },
+      } = await axios.get<Response>(
+        "https://api.instantwebtools.net/v1/passenger",
+        {
+          params,
+        }
+      );
+      setTotalPages(totalPages);
+      setItems(data);
+    };
+    fetch();
+  }, [page]);
+  return (
+    <AppWrapper className="App">
+      <PageTitle>Passenger Info</PageTitle>
+      <UserList>
+        {items.map((item, idx) => (
+          <UserCard
+            key={idx}
+            name={item.name}
+            trip={item.trips}
+            airline={item.airline[0].name}
+          />
+        ))}
+      </UserList>
+      <PaginationWrapper>
+        <Pagination
+          count={totalPages}
+          page={page}
+          onPageChange={handlePageChange}
+        />
+      </PaginationWrapper>
+    </AppWrapper>
+  );
 }
 
 export default App;
--- pagination/src/components/Pagination.tsx
+++ pagination/src/components/Pagination.tsx
@@ -1,3 +1,83 @@
 import styled from "@emotion/styled/macro";
 import { css } from "@emotion/react";
-import {} from "react-icons/ai";
+import { GrFormPrevious, GrFormNext } from "react-icons/gr";
+import { AiOutlineEllipsis } from "react-icons/ai";
+import { PaginationProps } from "../interface/PaginationProps";
+import usePagination from "../hooks/usePagination";
+
+const Navigation = styled.nav`
+  padding: 10px;
+`;
+
+const Button = styled.button<{ selected?: boolean }>`
+  color: ${({ selected }) => (selected ? "#fff" : "#000")};
+  border: 0;
+  margin: 0;
+  padding: 8px 12px;
+  font-size: 16px;
+  font-weight: normal;
+  background-color: ${({ selected }) => (selected ? "#36dafa" : "#fff")};
+  cursor: pointer;
+  border-radius: 100%;
+  width: 48px;
+  height: 48px;
+  &:hover {
+    background-color: #ccc;
+    color: #fff;
+  }
+  &:active {
+    opacity: 0.8;
+  }
+`;
+
+const Item = styled.li``;
+
+const ItemList = styled.ul`
+  margin: 0;
+  padding: 0;
+  display: flex;
+  flex-wrap: wrap;
+  list-style: none;
+  ${Item} + ${Item} {
+    margin-left: 8px;
+  }
+`;
+
+const Pagination: React.FC<PaginationProps> = ({
+  count,
+  page,
+  onPageChange,
+  disabled,
+  siblingCount,
+  boundaryCount,
+}) => {
+  const getLabel = (item: number | string) => {
+    if (typeof item === "number") return item;
+    else if (item.indexOf("ellipsis") > -1) return <AiOutlineEllipsis />;
+    else if (item.indexOf("prev") > -1) return <GrFormPrevious />;
+    else if (item.indexOf("next") > -1) return <GrFormNext />;
+  };
+  const { items } = usePagination({
+    count,
+    page,
+    onPageChange,
+    disabled,
+    siblingCount,
+    boundaryCount,
+  });
+  return (
+    <Navigation>
+      <ItemList>
+        {items.map(({ key, disabled, selected, onClick, item }) => (
+          <Item key={key}>
+            <Button disabled={disabled} selected={selected} onClick={onClick}>
+              {getLabel(item)}
+            </Button>
+          </Item>
+        ))}
+      </ItemList>
+    </Navigation>
+  );
+};
+
+export default Pagination;
