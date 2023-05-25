3b3a910e202e9df27da0fc26d627b03de9c8d35a
jadekang0611 <56938630+jadekang0611@users.noreply.github.com>
2023-05-25 12:32:58 -0700

    feat: implement modal component and demo w/o transition effect

---
+++ modal/src/components/Modal.tsx
@@ -0,0 +1,55 @@
+import styled from "@emotion/styled/macro";
+import { ReactNode } from "react";
+import Portal from "./Portal";
+
+const Overlay = styled.div`
+  position: fixed;
+  z-index: 10;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  overflow: hidden;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Dim = styled.div`
+  position: absolute;
+  top: 0;
+  right: 0;
+  left: 0;
+  bottom: 0;
+  background-color: rgba(0, 0, 0, 0.5);
+`;
+
+const Container = styled.div`
+  max-width: 456px;
+  position: relative;
+  width: 100%;
+`;
+
+interface Props {
+  children: ReactNode;
+  isOpen: boolean;
+  onClose: () => void;
+  selector?: string;
+}
+
+const Modal: React.FC<Props> = ({ children, isOpen, onClose, selector }) => {
+  return (
+    <>
+      {isOpen ? (
+        <Portal selector={selector}>
+          <Overlay>
+            <Dim onClick={onClose} />
+            <Container>{children}</Container>
+          </Overlay>
+        </Portal>
+      ) : null}
+    </>
+  );
+};
+
+export default Modal;
