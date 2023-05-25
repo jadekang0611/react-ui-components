acb5e868a34ea39c514c0db8f1fa3719fca3e76b
jadekang0611 <56938630+jadekang0611@users.noreply.github.com>
2023-05-25 16:23:21 -0700

    feat: implement infinite scroll ui with api

--- infinite-scroll-v1/src/App.tsx
+++ infinite-scroll-v1/src/App.tsx
@@ -1,7 +1,82 @@
-import React from "react";
+import React, { useEffect, useState, useRef } from "react";
+import axios from "axios";
+import styled from "@emotion/styled/macro";
+import { throttle } from "throttle-debounce";
+
+import { Passenger, Response } from "./interface/Api";
+
+const ListItem = styled.li`
+  font-size: 36px;
+`;
+
+const List = styled.ul`
+  overflow-x: hidden;
+  overflow-y: scroll;
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  width: 100%;
+  height: 512px;
+  ${ListItem} + ${ListItem} {
+    margin-top: 12px;
+  }
+`;
 
 function App() {
-  return <div className="App"></div>;
+  const scrollRef = useRef<HTMLUListElement | null>(null);
+  const pageRef = useRef<number>(0);
+
+  const [items, setItems] = useState<Array<Passenger>>([]);
+  const [isLast, setIsLast] = useState<boolean>(false);
+  const [isScrollBottom, setIsScrollBottom] = useState<boolean>(false);
+
+  const handleScroll = throttle(1000, () => {
+    if (scrollRef.current) {
+      const { scrollHeight, offsetHeight, scrollTop } = scrollRef.current;
+
+      const offset = 50;
+
+      setIsScrollBottom(scrollHeight - offsetHeight - scrollTop < offset);
+    }
+  });
+
+  const fetch = async (init?: boolean) => {
+    const params = { size: 30, page: pageRef.current };
+
+    try {
+      const res = await axios.get<Response>(
+        "https://api.instantwebtools.net/v1/passenger",
+        { params }
+      );
+
+      setItems(init ? res.data.data : items.concat(res.data.data));
+      setIsLast(res.data.totalPages === pageRef.current);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  useEffect(() => {
+    if (isScrollBottom) {
+      pageRef.current = pageRef.current + 1;
+
+      !isLast && fetch();
+    }
+  }, [isScrollBottom, isLast]);
+
+  useEffect(() => {
+    fetch(true);
+  }, []);
+
+  return (
+    <div>
+      <List ref={scrollRef} onScroll={handleScroll}>
+        {items.map((item) => (
+          <ListItem key={item._id}>{item.name}</ListItem>
+        ))}
+      </List>
+    </div>
+  );
 }
 
 export default App;
